26c2ecbdd7f9b560a570fa548826811781e6178b
doxin <lieuwemo@gmail.com>
2011-10-11 23:01:05 +0200

    setup script fixes

--- Setup.php
+++ Setup.php
@@ -3,14 +3,27 @@ require_once("Global.php");
 require_once("Settings.php");
 global $Database;
 $con = StartDatabase();
-$Query=file_get_contents("dump.sql");
-$Result=mysql_query($Query,$con);
-echo gettype($Result)."<br>";
-if($Result)
+$Queries=explode(file_get_contents("dump.sql"), ";");
+array_pop($Queries);
+$good=True;
+foreach($Queries as $Query)
+{
+    $Result=mysql_query($Query,$con);
+    if($result)
+        echo "";
+    else
+        {
+        echo mysql_error()."<br>";
+        $good=false;
+        }
+    
+}
+
+if($good)
     echo "sucessfull undump";
 else
 {
-    echo "whoops! something went wrong.<br>mysql gave \"".mysql_error($con)."\"";
+    echo "whoops! something went wrong.";
 }
 CloseDatabase($con);
 ?>
--- dump.sql
+++ dump.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 11, 2011 at 07:08 PM
--- Server version: 5.1.54
--- PHP Version: 5.3.5-1ubuntu7.2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `tpt_server`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
-
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sessionid` varchar(31) CHARACTER SET utf8 NOT NULL,
@@ -33,13 +5,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `expiredate` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `sessionid` (`sessionid`,`userid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+);
 
 CREATE TABLE IF NOT EXISTS `users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -50,8 +16,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `passhash` (`passhash`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+);
