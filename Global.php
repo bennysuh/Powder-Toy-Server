0c4a86b669ddd37cad5e90c7ca26e94e8af46c57
Cr15py <christopherpople@gmail.com>
2011-10-12 12:43:39 -0300

    Fixed LogEvent(). May need permissions to be 777 though :$

--- Global.php
+++ Global.php
@@ -11,8 +11,10 @@ function StartDatabase(){
 function CloseDatabase($Connection){
 	mysql_close($Connection);
 }
-/*
+
 function LogEvent($Event, $UserId, $Connection) { //Event is the log message. E.g. 
+	if(!isset($Event)) return 0;
+	if(!isset($UserId)) $UserId = 1;
 	$Year = date("Y");
 	$Month = date("F");
 	$Day = date("l");
@@ -24,17 +26,18 @@ function LogEvent($Event, $UserId, $Connection) { //Event is the log message. E.
 	$FileHandle = fopen($FileName, 'a') or die("Can't open file!");
 	$UserId = mysql_real_escape_string($UserId);
 	$QueryData = mysql_query("SELECT name FROM users WHERE id = '".$UserID."';", $Connection);
-	$Row = mysql_fetch_array($QueryData)
+	$Row = mysql_fetch_array($QueryData);
 	$UserName = $Row["name"];
 	mysql_free_result($QueryData);
 	$LogData = date("d/m/Y h:i:s A")." | ".$UserName." ".$Event."\n";
 	fwrite($FileHandle, $LogData);
 	fclose($ourFileHandle);
-}*/
+}
 
-/*function IdToName($Id, $Connection){
-	return mysql_fetch_array(mysql_query("SELECT name FROM users WHERE id = ".mysql_real_escape_string($Id).";", $Connection))["name"];
-}*/
+function IdToName($Id, $Connection){
+	$Row = mysql_fetch_array(mysql_query("SELECT name FROM users WHERE id = ".mysql_real_escape_string($Id).";", $Connection));
+	return $Row["name"];
+}
 
 function CreateSessionId()
 {
