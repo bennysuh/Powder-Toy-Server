51a4adcb0655138e1598cfab1c63430be8e72100
Cr15py <christopherpople@gmail.com>
2011-10-12 15:47:56 -0300

    Edited Settings.php via GitHub

--- Settings.php
+++ Settings.php
@@ -1,8 +1,8 @@
 <?php
 global $Database;
 $Database["Username"] = "root"; //Only change this if you are using a different user.
-$Database["Password"] = "25010150"; //Change this.
-$Database["Database"] = "PowderServer"; //Only change this if you know that you are to be using another database.
+$Database["Password"] = "<your_passsword>"; //Change this.
+$Database["Database"] = "mysql"; //Only change this if you know that you are to be using another database.
 $Database["Host"]     = "127.0.0.1"; //Only change this if you are using a separate server for the database.
 
 ?>
