21d91cb347a86042740418c768b893c20400a4b9
doxin <lieuwemo@gmail.com>
2011-10-12 18:30:55 +0200

    updated database for permissions

--- dump.sql
+++ dump.sql
@@ -18,3 +18,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `passhash` (`passhash`)
 );
+
+CREATE TABLE IF NOT EXISTS `permissions<>users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `permissionID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `name` (`name`)
+);
+
