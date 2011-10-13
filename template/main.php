4a54540a2f6d9151828c5ae83de92fd7e0754fa0
doxin <lieuwemo@gmail.com>
2011-10-13 20:02:14 +0200

    started work on templating

---
+++ template/main.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<style>
+#content{border:5px solid black; border-radius:16px; position:absolute; left:25%; right:25%; padding:16px; top:128px;}
+</style>
+</head>
+<body>
+    <div id=content>
+    <?PHP echo $Content; ?>
+    </div>
+</body>
+</html>
