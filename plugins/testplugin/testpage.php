e0611d0fd1c8443535323cdbd6fb5204a0a84368
Cr15py <christopherpople@gmail.com>
2011-10-19 21:15:00 -0300

    Same.

--- plugins/testplugin/testpage.php
+++ plugins/testplugin/testpage.php
@@ -1,4 +1,4 @@
 <?PHP
 $Content="if you see this page it means that both the plugin system and the template system are operational.";
-require_once("template/main.php");
+require_once("../../template/main.php");
 ?>
