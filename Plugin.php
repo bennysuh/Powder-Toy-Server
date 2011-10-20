a5accd14c0d657ea8fee8a9725496084fa0d197d
Cr15py <christopherpople@gmail.com>
2011-10-19 21:05:55 -0300

    Fixed spelling/grammar/punctuation. Added isset checks. Not to pester you but be sure that you check whether a variable has been passed. Especially with $_POST and $_GET. Just a reminder :)

--- Plugin.php
+++ Plugin.php
@@ -1,17 +1,21 @@
 <?PHP
+if(!isset($_GET['plugin']))
+    return -1;
 $plugin=$_GET['plugin'];
+if(!isset($_GET['filename']))
+	return -1;
 $filename=$_GET['filename'];
 $i=0;
 $i+=substr_count($plugin,"/");
 $i+=substr_count($filename,"\\");
 if($i)
-    die("i'm afraid that that isn't allowed. not only is it against the TOS it's against the law. your ip is logged and a local police team dispatched to your location.");
+    die("I'm afraid that that isn't allowed. Not only is it against the TOS, it's against the law. Your IP has been logged and police have been contacted.");
 $i=0;
 $i+=substr_count($filename,"/");
 $i+=substr_count($filename,"\\");
 if($i)
-    die("i'm afraid that that isn't allowed. not only is it against the TOS it's against the law. your ip is logged and a local police team dispatched to your location.");
-$scriptdir= substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'));
+    die("I'm afraid that that isn't allowed. Not only is it against the TOS, it's against the law. Your IP has been logged and police have been contacted.");
+$scriptdir= substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], './'));
 $incfile="$scriptdir/plugins/$plugin/$filename";
 if(is_readable($incfile))
     require_once($incfile);
